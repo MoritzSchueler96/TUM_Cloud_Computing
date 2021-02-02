0e38506f0ea0d422057c08ee97d5c699b5434de1
MoritzSchueler96 <47536226+MoritzSchueler96@users.noreply.github.com>
2021-02-02 19:06:49 +0100

    update api's

--- exercises/ex05/openwhisk_parallel_composer/test_ow_function.js
+++ exercises/ex05/openwhisk_parallel_composer/test_ow_function.js
@@ -3,30 +3,30 @@
 function main(params) {
     // eslint-disable-next-line global-require, import/no-extraneous-dependencies
     const options = {
-        apihost: ':31001',
-        api_key: '23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP',
-        ignore_certs: true
-    }
+        apihost: '35.195.41.79:31001',
+        api_key:
+            '23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP',
+        ignore_certs: true,
+    };
     const ow = require('openwhisk')(options);
 
     let data = {
-        "$composer": {
-            "redis": {
-                "uri": "redis://owdev-redis.openwhisk.svc.cluster.local:6379"
+        $composer: {
+            redis: {
+                uri: 'redis://owdev-redis.openwhisk.svc.cluster.local:6379',
             },
-            "openwhisk": {
-                "ignore_certs": true
-            }
+            openwhisk: {
+                ignore_certs: true,
+            },
         },
-        "name": "anshul",
-        "product_id": 2,
-        "mongo_host_ip": "",
-        "db": "ccs",
-        "collection": "products",
-        "mongo_user": "root",
-        "mongo_pass": "",
-        
-    }
+        name: 'CCS',
+        product_id: 2,
+        mongo_host_ip: '138.246.233.141',
+        db: 'ccs',
+        collection: 'products',
+        mongo_user: 'root',
+        mongo_pass: 'ccs_root_Pass_7631',
+    };
     const invoke = (actionName, params) => ow.actions.invoke({ actionName, params, blocking: true });
 
     return invoke('composition', data)
